c22d534a28a1445218dc22624ffa0534a51c8d1a
Apulu123 <alex41610@gmail.com>
2023-08-20 20:54:11 +0800

    ver 1

---
+++ w2/form.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.forms import ModelForm, TextInput, EmailInput, PasswordInput
+from django.contrib.auth.models import User
+from .models import UserProfile
+
+
+class UserInfoForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'password': forms.TextInput(attrs={'class': 'form-control'})
+        }
+
+        labels = {
+            'username': '使用者名稱',
+            'email': '電子郵件',
+            'password': '密碼'
+        }
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(label='帳號')
+    password = forms.CharField(label='密碼', widget=forms.PasswordInput)
